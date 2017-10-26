a39ec3c4d6211057b865abf016cb0eb0d978bbe0
NIKHIL GANJI <nikhilganji@users.noreply.github.com>
2017-10-26 14:01:12 -0700

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDRM8gqqyZ2ZdX5gj8ysTcmwpFORQr95i8",
+    authDomain: "oshop-nik.firebaseapp.com",
+    databaseURL: "https://oshop-nik.firebaseio.com",
+    projectId: "oshop-nik",
+    storageBucket: "oshop-nik.appspot.com",
+    messagingSenderId: "636778426893"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDRM8gqqyZ2ZdX5gj8ysTcmwpFORQr95i8",
+    authDomain: "oshop-nik.firebaseapp.com",
+    databaseURL: "https://oshop-nik.firebaseio.com",
+    projectId: "oshop-nik",
+    storageBucket: "oshop-nik.appspot.com",
+    messagingSenderId: "636778426893"
+  }
+};
